1c0b26e485c5b922ebc95682f8cbcff616ee1e59
kuro48 <kuroryu.basket@gmail.com>
2019-03-07 08:23:49 +0000

    upload to Heroku

--- models.rb
+++ models.rb
@@ -5,30 +5,30 @@ if development?
   ActiveRecord::Base.establish_connection("sqlite3:db/development.db")
 end
 
-class User < ActiveRecord::Base
-  has_secure_password
-  validates :password,
-    presence: {on: create}
+# class User < ActiveRecord::Base
+#   has_secure_password
+#   validates :password,
+#     presence: {on: create}
 
-  validates :email,
-    uniqueness: true,
-    presence: true
-    # fomat: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+#   validates :email,
+#     uniqueness: true,
+#     presence: true
+#     # fomat: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
 
-  validates :nickname,
-    uniqueness: true,
-    presence: true
+#   validates :nickname,
+#     uniqueness: true,
+#     presence: true
 
-  validates :username,
-    uniqueness: true,
-    presence: true
+#   validates :username,
+#     uniqueness: true,
+#     presence: true
 
-  has_many :posts
-end
+#   has_many :posts
+# end
 
-class Post < ActiveRecord::Base
-  validates :text,
-  presence: true
+# class Post < ActiveRecord::Base
+#   validates :text,
+#   presence: true
 
-  belongs_to :user
-end
+#   belongs_to :user
+# end
